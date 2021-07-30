ee7620494535582d8a7a66026ea578449ad0a98c
Bilwin <marf.zpd@gmail.com>
2021-07-30 22:21:33 +0900

    Update Game.cs

--- code/Game.cs
+++ code/Game.cs
@@ -1,105 +1,113 @@
 ﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
-[Library( "hillmons", Title = "Hillmons" )]
-partial class Hillmons : Game
+namespace Hillmons
 {
-	public HillmonsKernel()
+	[Library("minimal")]
+	public partial class Hillmons : Sandbox.Game
 	{
-		if ( IsServer )
+		public HillmonsKernel()
 		{
-			_ = new HillmonsHud();
-		}
-	}
-
-	public override void ClientJoined( Client cl )
-	{
-		base.ClientJoined( cl );
-		var player = new HillmonsPlayer();
-		player.Respawn();
-
-		cl.Pawn = player;
-	}
-
-	protected override void OnDestroy()
-	{
-		base.OnDestroy();
-	}
-
-	[ServerCmd( "spawn" )]
-	public static void Spawn( string modelname )
-	{
-		var owner = ConsoleSystem.Caller?.Pawn;
-
-		if ( ConsoleSystem.Caller == null )
-			return;
-
-		var tr = Trace.Ray( owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 500 )
-			.UseHitboxes()
-			.Ignore( owner )
-			.Size( 2 )
-			.Run();
-
-		var ent = new Prop();
-		ent.Position = tr.EndPos;
-		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRot.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
-		ent.SetModel( modelname );
-
-		// Drop to floor
-		if ( ent.PhysicsBody != null && ent.PhysicsGroup.BodyCount == 1 )
-		{
-			var p = ent.PhysicsBody.FindClosestPoint( tr.EndPos );
-
-			var delta = p - tr.EndPos;
-			ent.PhysicsBody.Position -= delta;
-			//DebugOverlay.Line( p, tr.EndPos, 10, false );
+			if (IsServer)
+			{
+				_ = new HillmonsHud();
+			}
 		}
 
-	}
-
-	[ServerCmd( "spawn_entity" )]
-	public static void SpawnEntity( string entName )
-	{
-		var owner = ConsoleSystem.Caller.Pawn;
-
-		if ( owner == null )
-			return;
-
-		var attribute = Library.GetAttribute( entName );
-
-		if ( attribute == null || !attribute.Spawnable )
-			return;
-
-		var tr = Trace.Ray( owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 200 )
-			.UseHitboxes()
-			.Ignore( owner )
-			.Size( 2 )
-			.Run();
-
-		var ent = Library.Create<Entity>( entName );
-		if ( ent is BaseCarriable && owner.Inventory != null )
+		public override void ClientJoined(Client cl)
 		{
-			if ( owner.Inventory.Add( ent, true ) )
+			base.ClientJoined(cl);
+			var player = new HillmonsPlayer();
+			player.Respawn();
+
+			cl.Pawn = player;
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+		}
+
+		[ServerCmd("spawn")]
+		public static void Spawn(string modelname)
+		{
+			var owner = ConsoleSystem.Caller?.Pawn;
+
+			if (ConsoleSystem.Caller == null)
 				return;
+
+			var tr = Trace.Ray(owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 500)
+				.UseHitboxes()
+				.Ignore(owner)
+				.Size(2)
+				.Run();
+
+			var ent = new Prop();
+			ent.Position = tr.EndPos;
+			ent.Rotation = Rotation.From(new Angles(0, owner.EyeRot.Angles().yaw, 0)) * Rotation.FromAxis(Vector3.Up, 180);
+			ent.SetModel(modelname);
+
+			// Drop to floor
+			if (ent.PhysicsBody != null && ent.PhysicsGroup.BodyCount == 1)
+			{
+				var p = ent.PhysicsBody.FindClosestPoint(tr.EndPos);
+
+				var delta = p - tr.EndPos;
+				ent.PhysicsBody.Position -= delta;
+				//DebugOverlay.Line( p, tr.EndPos, 10, false );
+			}
+
 		}
 
-		ent.Position = tr.EndPos;
-		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRot.Angles().yaw, 0 ) );
-	}
-
-	public override void DoPlayerNoclip( Client player )
-	{
-		if ( player.Pawn is Player basePlayer )
+		[ServerCmd("spawn_entity")]
+		public static void SpawnEntity(string entName)
 		{
-			if ( basePlayer.DevController is NoclipController )
+			var owner = ConsoleSystem.Caller.Pawn;
+
+			if (owner == null)
+				return;
+
+			var attribute = Library.GetAttribute(entName);
+
+			if (attribute == null || !attribute.Spawnable)
+				return;
+
+			var tr = Trace.Ray(owner.EyePos, owner.EyePos + owner.EyeRot.Forward * 200)
+				.UseHitboxes()
+				.Ignore(owner)
+				.Size(2)
+				.Run();
+
+			var ent = Library.Create<Entity>(entName);
+			if (ent is BaseCarriable && owner.Inventory != null)
 			{
-				Log.Info( "Noclip Mode Off" );
-				basePlayer.DevController = null;
+				if (owner.Inventory.Add(ent, true))
+					return;
 			}
-			else
+
+			ent.Position = tr.EndPos;
+			ent.Rotation = Rotation.From(new Angles(0, owner.EyeRot.Angles().yaw, 0));
+		}
+
+		public override void DoPlayerNoclip(Client player)
+		{
+			if (player.Pawn is Player basePlayer)
 			{
-				Log.Info( "Noclip Mode On" );
-				basePlayer.DevController = new NoclipController();
+				if (basePlayer.DevController is NoclipController)
+				{
+					Log.Info("Noclip Mode Off");
+					basePlayer.DevController = null;
+				}
+				else
+				{
+					Log.Info("Noclip Mode On");
+					basePlayer.DevController = new NoclipController();
+				}
 			}
 		}
 	}
+
 }
